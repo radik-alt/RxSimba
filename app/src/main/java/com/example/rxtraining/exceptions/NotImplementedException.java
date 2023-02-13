3b4f5e372edad00cc1be9ad3dc67b674b02a214b
Radik <Radik.app@yandex.ru>
2023-02-14 03:12:57 +0400

    Initial commit

---
+++ app/src/main/java/com/example/rxtraining/exceptions/NotImplementedException.java
@@ -0,0 +1,8 @@
+package com.example.rxtraining.exceptions;
+
+/**
+ * @author Arthur Korchagin (artur.korchagin@simbirsoft.com)
+ * @since 13.11.18
+ */
+public class NotImplementedException extends RuntimeException {
+}
